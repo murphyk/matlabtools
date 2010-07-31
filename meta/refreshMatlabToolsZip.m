e0b4747eeb0e29bf8762263b1899b7d25ec47c3e
mattbdunham <mattbdunham@f338f684-022f-11df-897e-95fb7fca2173>
2010-07-31 17:11:54 +0000

    git-svn-id: http://matlabtools.googlecode.com/svn/trunk@13 f338f684-022f-11df-897e-95fb7fca2173

--- meta/refreshMatlabToolsZip.m
+++ meta/refreshMatlabToolsZip.m
@@ -3,19 +3,22 @@ function refreshMatlabToolsZip()
 % Requires an svn installation, (not just e.g. tortoise svn)
 % All files must be checked into svn
 %%
-fprintf('removing old zip file...'); 
-destZip = fullfile(matlabToolsRoot(), 'matlabTools.zip'); 
-if exist(destZip, 'file')
-    delete(destZip); 
-end
-fprintf('done\n');
+
 tmpRoot = tempname();
 mkdir(tmpRoot); 
 tmpPath = fullfile(tmpRoot, 'matlabTools');
 fprintf('Exporting code..........'); 
 system(sprintf('svn export %s %s', matlabToolsRoot(), tmpPath));
-
+%%
+fprintf('removing old zip file...'); 
+oldZip = fullfile(tmpPath, 'matlabTools.zip'); 
+if exist(oldZip, 'file')
+    delete(oldZip); 
+end
+fprintf('done\n');
+%%
 fprintf('zipping.................');
+destZip = fullfile(matlabToolsRoot(), 'matlabTools.zip'); 
 zip(destZip, fullfile(tmpRoot, 'matlabTools', '*.*')); 
 fprintf('done\ncleaning up.............'); 
 system(sprintf('rmdir /Q /S %s', tmpPath));
