5c067379947c3f6a49bf6488d8a77c9c9a1307a4
mattbdunham <mattbdunham@f338f684-022f-11df-897e-95fb7fca2173>
2010-08-11 19:25:48 +0000

    git-svn-id: http://matlabtools.googlecode.com/svn/trunk@52 f338f684-022f-11df-897e-95fb7fca2173

--- metaTools/compileC.m
+++ metaTools/compileC.m
@@ -1,19 +1,31 @@
-function compileC()
-% Try and compile all mexable c-files in the pmtk directory structure
+function compileC(root, excludedDirs)
+% Try and compile all mexable c-files in the directory structure that
+% have not already been compiled. 
 % PMTKneedsMatlab 
-cd(pmtk3Root());
-exclusions = {};
-cfiles = cfilelist();
-for i=1:numel(exclusions)
-    cfiles = setdiff(cfiles, cfilelist(exclusions{i}));
+%%
+if nargin < 1, root = pmtk3Root(); end
+
+if nargin < 2
+    excludedDirs = {'lightspeed2.3'  % use install_lightspeed or installLightspeedPMTK
+                    'fastfit'        % use install_fastfit
+                   };
 end
-cfiles = setdiff(cfiles, cfilelist());
+cfiles = cfilelist(root);
+for i=1:numel(excludedDirs)
+   ex = cellfun(@(c)isSubstring(excludedDirs{i}, c), cfiles); 
+   cfiles(ex) = []; 
+end
+
+
 for j=1:numel(cfiles)
     try
-        cfile = which(cfiles{j});
-        cd(fileparts(cfile));
-        fprintf('Compiling %s\n',cfile);
-        mex(cfile);
+        cfile = cfiles{j};
+        fname = fnameOnly(cfile);
+        if ~exist(fname, 'file') == 3
+            cd(fileparts(cfile));
+            fprintf('Compiling %s\n',cfile);
+            mex(cfile);
+        end
     catch ME
         fprintf('Could not compile %s\n', cfile);
     end
