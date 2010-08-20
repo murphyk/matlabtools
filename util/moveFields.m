d842016e6a729b46e566bf70da2d7b22b7533522
mattbdunham <mattbdunham@f338f684-022f-11df-897e-95fb7fca2173>
2010-08-20 15:55:07 +0000

    git-svn-id: http://matlabtools.googlecode.com/svn/trunk@66 f338f684-022f-11df-897e-95fb7fca2173

---
+++ util/moveFields.m
@@ -0,0 +1,13 @@
+function [S1, S2] = moveFields(fields, S1, S2)
+%% Move fields from struct S1 to struct S2
+
+if nargin < 3
+    S2 = struct();
+end
+for i=1:numel(fields)
+   f      = fields{i};
+   S2.(f) = S1.(f); 
+   S1     = rmfield(S1, f); 
+end
+   
+end
