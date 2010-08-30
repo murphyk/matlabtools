0392e362a76f307a6ff9f21245bb58e020516a6d
mattbdunham <mattbdunham@f338f684-022f-11df-897e-95fb7fca2173>
2010-08-30 17:14:23 +0000

    git-svn-id: http://matlabtools.googlecode.com/svn/trunk@96 f338f684-022f-11df-897e-95fb7fca2173

---
+++ util/addToStruct.m
@@ -0,0 +1,13 @@
+function S = addToStruct(S, varargin)
+%% Add variables to a struct using their names in the current scope
+% See also structure.m
+%
+% Caution - this will overwrite any existing fields by the input names. 
+% *** does not support anonymous variables as in addToStruct(S, 25, 2+3), etc ***
+% 
+%%
+for i=2:nargin
+    S.(inputname(i)) = varargin{i-1};
+end
+end
+
