b9a7e166149bbb4133168bea86d8b4e9cc7f7ea5
mattbdunham <mattbdunham@f338f684-022f-11df-897e-95fb7fca2173>
2010-09-09 05:53:31 +0000

    git-svn-id: http://matlabtools.googlecode.com/svn/trunk@119 f338f684-022f-11df-897e-95fb7fca2173

---
+++ meta/changeCopyrightMatlabTools.m
@@ -0,0 +1,14 @@
+function changeCopyrightMatlabTools(oldStr, newStr, fname)
+%% Modify the copyright line in every matlabTools file
+% or in just the specified file. The oldStr should be exactly as it appears
+% in the file(s), including case, spaces, and leading '%' chars.
+%%
+if nargin > 2
+    searchAndReplaceLine(fname, oldStr, newStr);
+else
+    flist = filelist(matlabToolsRoot(), '*.m', true);
+    for i=1:numel(flist)
+        searchAndReplaceLine(flist{i}, oldStr, newStr);
+    end
+end
+end
---
+++ meta/insertCopyrightMatlabTools.m
@@ -0,0 +1,15 @@
+function insertCopyrightMatlabTools(fname)
+%% Insert a copyright notice into every matlabTools .m file
+% or into just the specified file.
+%%
+text = 'This file is from matlabtools.googlecode.com';
+
+if nargin > 0
+    insertCopyright(text, fname);
+else
+    flist = filelist(matlabToolsRoot(), '*.m', true);
+    for i=1:numel(flist)
+        insertCopyright(text, flist{i});
+    end
+end
+end
