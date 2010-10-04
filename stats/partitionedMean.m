42075004bc9ee2296bd8c47ac94d6139bb72d781
murphyk2 <murphyk2@f338f684-022f-11df-897e-95fb7fca2173>
2010-10-04 15:28:12 +0000

    git-svn-id: http://matlabtools.googlecode.com/svn/trunk@124 f338f684-022f-11df-897e-95fb7fca2173

--- stats/partitionedMean.m
+++ stats/partitionedMean.m
@@ -9,17 +9,6 @@ function [M, counts] = partitionedMean(X, y, C)
 % counts(i) = sum(y==i)
 %
 % See also partitionedSum
-%%
-% This is a vectorized version of this code fragment:
-%
-% M = zeros(C, d);
-% for c=1:C
-%     ndx = find(y==c); 
-%     M(c, :) = mean(X(ndx, :));
-% end
-% 
-% counts = histc(y, 1:C);
-%%
 
 % This file is from matlabtools.googlecode.com
 
@@ -27,14 +16,25 @@ function [M, counts] = partitionedMean(X, y, C)
 if nargin < 3
     C = nunique(y);
 end
-if isOctave
-    S = bsxfun(@eq, (1:C)', y');
+if 1
+  d = size(X,2);
+  M = zeros(C, d);
+  for c=1:C
+    ndx = (y==c);
+    M(c, :) = mean(X(ndx, :));
+  end
+  counts = histc(y, 1:C);
 else
+  % fancy vectorized version
+  if isOctave
+    S = bsxfun(@eq, (1:C)', y');
+  else
     S = bsxfun(@eq, sparse(1:C)', y');       % C-by-n logical sparse matrix, (basically a one-of-K encoding transposed)
+  end
+  M = S*X;                                 % computes the sum, yielding a C-by-d matrix
+  counts = histc(y, 1:C);
+  M = bsxfun(@rdivide, M, counts);         % divide by counts to get mean
 end
-M = S*X;                                 % computes the sum, yielding a C-by-d matrix
-counts = histc(y, 1:C);                  
-M = bsxfun(@rdivide, M, counts);         % divide by counts to get mean
 
 end
 
